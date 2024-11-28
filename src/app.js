4367a6526ce271ea1733b2209d381514dca75f01
Mudit Maheshwari <140621191+mudit06mah@users.noreply.github.com>
2024-11-28 18:09:14 +0530

    backend setup completed-p3

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express, { urlencoded } from "express";
+import cors from "cors"
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials:true
+    })
+)
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended: true, limit:"16kb"}));
+app.use(express.static("public"));
+
+//import routes:
+import userRoutes from "./routes/user.routes.js";
+
+//declare routes:
+app.use("/api/v1/user",userRoutes);
+
+export {app}
